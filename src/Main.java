676bb05d81bec8fb7dbca29f331d0e3ce85a6e3a
samudoria <samueledoria@gmail.com>
2020-10-14 18:19:29 +0200

    Added sum of two random numbers and scanner to add the max of the range

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
+    Random random = new Random();
+    Scanner sc = new Scanner(System.in);
 
+    public static void main(String args[]){
         int[] numbers = new int[]{5,4,8,9,6,3,7,4,2,85,63,45,10};
-        Random random = new Random();
         int ranNum = random.nextInt(numbers.length-1);
         int a = numbers[ranNum];
         System.out.print("Your generated random number is:  ");
         System.out.println(a);
+        System.out.println("Enter a number and I will generate a random number in its range");
+        int max = sc.nextInt();
+        System.out.println("Random is " + max);
+        System.out.println("Sum of two random numbers generated from it");
+        System.out.println(sum(max));
+    }
 
+    public static int sum(int maxNum){
+        int a = random.nextInt(maxNum), b = random.nextInt(maxNum);
+        return a + b ;	
     }
 }
